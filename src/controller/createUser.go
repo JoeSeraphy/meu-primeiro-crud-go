ae3ba597621c4572b3c7edd0377f8f7e8b6dff7b
joelson Silva <joelson.s@outlook.com>
2026-04-25 19:59:28 -0300

    validando campos

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/joeseraphy/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/joeseraphy/meu-primeiro-crud-go/src/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("there are some incorrect filds, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
+}
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joeseraphy/meu-primeiro-crud-go/src/controller"
+)
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userID")
-	r.GET("/getUserByEmail/:userEmail")
-	r.POST("/createUser")
-	r.PUT("/updateUser/:userID")
-	r.DELETE("/deleteUser/:userID")
+	r.GET("/getUserById/:userID", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userID", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userID", controller.DeleteUser)
 }
